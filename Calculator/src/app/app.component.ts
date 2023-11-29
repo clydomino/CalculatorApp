9bc768f71dde0f18203d8ea7356477e4b5acbdf2
clydomino <clydomino@gmail.com>
2023-11-29 12:16:10 -0500

    reset

--- Calculator/src/app/app.component.ts
+++ Calculator/src/app/app.component.ts
@@ -11,84 +11,9 @@ export class AppComponent {
   //what user types with the keyboard
   userinput = '';
 
-  //operation parts
-  leftoperand = 0;
-  rightoperand = 0;
-  operator = ''
-
-  //term
-  term = "";
-
-  //result of the calculation
-  result = 0;
-
-  mouseonresult : boolean = false;
-  mouseclickonresult : boolean = false;
-
-
-  //change the operator
-  applyoperator(operator : string){
-    
-    this.updateTerms(operator);
-    /*if(this.rightoperand != 0){
-      this.calculate();
-      return ;
-    }*/
-    
-
-  }
-
-  //execute the calculation
-  calculate(){
-    this.rightoperand = +this.term
-    this.calloperator(this.operator);
-    this.term = "" + this.result;
-    this.leftoperand = +this.term;
-    this.rightoperand = 0;
-  }
-
-  //call the right operator according to the user input
-  calloperator(operator : string){
-
-    var left = Number(this.leftoperand);
-    var right = Number(this.rightoperand);
-
-    if(operator == "+"){
-      this.result = this.addition(left,right);
-    }
-
-    else if(operator == "-"){
-      this.result = this.substraction(left,right);
-    }
-
-    else if(operator == "x"){
-      this.result = this.multiplication(left,right);
-    }
-
-    else if(operator == "/"){
-      this.result = this.division(left,right);
-    }
+  //number on screen
+  term = '';
   
-  }
-
-  //Update terms of operation
-  updateTerms(operator : string){
-      this.operator = operator;
-
-      this.leftoperand = +this.term;
-       
-      this.term = "";
-            
-
-  }
-
-  //add digit to the term by clicking on a number button
-  addtermclick(t : string){
-    this.term += t;
-
-    this.mouseonresult = false;
-    this.mouseclickonresult = false;
-  }
 
   //add up the operands
   addition(a : number, b : number) : number{
@@ -114,14 +39,6 @@ export class AppComponent {
   //clear everything
   clear(){
 
-  }
-
-  //determine whether or not the cursor is focused on the term screen
-  clickdetected(){
-    if(this.mouseonresult)
-      this.mouseclickonresult = true;
-
-   // console.log(this.mouseonresult);
   }
 
 
@@ -160,9 +77,9 @@ export class AppComponent {
       console.log("erase");
     }
 
-    if(isFinite(Number(this.userinput)) && !this.mouseclickonresult){
+    /*if(isFinite(Number(this.userinput)) && !this.mouseclickonresult){
       this.term += this.userinput;
-    }
+    }*/
 
 
   }
